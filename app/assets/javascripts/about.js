0fbd66ddeb3d10bc835adfe5ced49739cf510ac6
Erin Beitel <erin.beitel@gmail.com>
2015-11-05 13:56:21 -0800

    Add drop down answer jquery functionality on FAQ.

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,6 +1,11 @@
+$(document).ready(function () { //Says don't load any of the jquery till the dom is ready for it. (html loads first)
+
 $(".question").click(function(){ // same as jQuery("#q1")
 		// console.log($("#q1")); // this console logs q1 when it's clicked.
+		console.log("Whohoo! about.js is connected!");
 		$(this).next().fadeToggle("fast");
 		$(this).children().toggleClass("collapse");
 
 })
+
+});
