d1a06d08f2c60775a6d500aaf4a624d680682192
Erin Beitel <erin.beitel@gmail.com>
2015-11-09 19:37:06 -0800

    Add local storage to persist list

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,4 +1,6 @@
 $(document).ready(function () { //Says don't load any of the jquery till the dom is ready for it.
+    $("#list-items").html(localStorage.getItem("listItems"));
+
     $(".add-items").submit(function(event) {
     		event.preventDefault(); // submit event prevented
     		
@@ -6,6 +8,7 @@ $(document).ready(function () { //Says don't load any of the jquery till the dom
 
     		if (item) {
     				$("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
+    				localStorage.setItem("listItems", $("#list-items").html()); //setItem is a method that is available on the localStorage API. Calling the key listItems. The value is the entire html block of the ul.
     				$("#todo-list-item").val(""); // clears out the text box after posting the to do item.
     		}
     });
@@ -21,5 +24,7 @@ $(document).ready(function () { //Says don't load any of the jquery till the dom
 		$(document).on("click", ".remove", function() {
 				$(this).parent().remove()
 		});
+
+
 
 });
