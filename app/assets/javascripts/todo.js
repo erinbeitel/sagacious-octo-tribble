7ece40b2ad5ce1d32644cfaf5dcd3e984c212b7e
Erin Beitel <erin.beitel@gmail.com>
2015-11-09 19:11:31 -0800

    add add and remove task functionality

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -4,8 +4,22 @@ $(document).ready(function () { //Says don't load any of the jquery till the dom
     		
     		var item = $("#todo-list-item").val(); //saving input item to variable. if item instead of var item it would be a global variable.
 
-    		$("#list-items").append("<li><input type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
-
-    		$("#todo-list-item").val(""); // clears out the text box after posting the to do item.
+    		if (item) {
+    				$("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
+    				$("#todo-list-item").val(""); // clears out the text box after posting the to do item.
+    		}
     });
+
+    // $(".checkbox").change(function(){
+    // 	console.log("Checkbox checked!");
+    // })
+
+		$(document).on("change", ".checkbox", function() {
+			$(this).parent().toggleClass("completed") //parent of the checkbox is the list item. could also use click instead of change.
+		});
+
+		$(document).on("click", ".remove", function() {
+				$(this).parent().remove()
+		});
+
 });
